39466cf152cb82d649355fa69ae23c4952182ef4
FelipeMartins <mfelipeluiz99@gmail.com>
2021-09-16 20:11:13 -0300

    requisito 5

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      name: value,
+    });
+  }
+
   render() {
+    const minName = 2;
+    const { name } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <form className="artistForm">
+            <input
+              onChange={ this.handleChange }
+              type="text"
+              name="artist"
+              data-testid="search-artist-input"
+            />
+            <button
+              disabled={ name.length < minName }
+              type="button"
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+            </button>
+          </form>
+        </form>
       </div>
     );
   }
